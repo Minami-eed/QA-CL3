e273fd3637fcaa0623810796bc19ed54941c728e
Minami <106195391+Minami-eed@users.noreply.github.com>
2023-11-20 22:49:03 -0500

    Add Service IClienteService

---
+++ src/main/java/pe/edu/cibertec/QACL3/Service/IClienteService.java
@@ -0,0 +1,17 @@
+package pe.edu.cibertec.QACL3.Service;
+
+import java.util.List;
+
+import pe.edu.cibertec.QACL3.Model.Cliente;
+
+public interface IClienteService {
+    public List<Cliente> listarClientes();
+
+    public Cliente obtenerCliente(Integer id);
+
+    public Cliente registrarCliente(Cliente cliente);
+
+    public Cliente modificarCliente(Cliente cliente);
+
+    public String eliminarCliente(Integer id);
+}
