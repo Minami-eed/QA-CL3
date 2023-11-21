0bd75620944b89df0378c7fe2fa0b34005986f16
Minami <106195391+Minami-eed@users.noreply.github.com>
2023-11-20 21:49:59 -0500

    Create new project

---
+++ src/main/java/pe/edu/cibertec/QACL3/QaCl3Application.java
@@ -0,0 +1,13 @@
+package pe.edu.cibertec.QACL3;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class QaCl3Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(QaCl3Application.class, args);
+	}
+
+}
---
+++ src/test/java/pe/edu/cibertec/QACL3/QaCl3ApplicationTests.java
@@ -0,0 +1,13 @@
+package pe.edu.cibertec.QACL3;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class QaCl3ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
